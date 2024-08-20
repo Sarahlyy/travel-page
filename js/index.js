cd43579b16ac9022fba442985dc4a602b868f905
Sarah Asare-Darko <saradarko22@gmail.com>
2024-08-20 07:20:52 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('btn-btn')
+btn.onclick = function () {
+    console.log('button has been clicked')
+    alert('More locations coming soon');
+}
+
+const button = document.getElementById('click-click')
+button.onclick = function () {
+    console.log('button has been clicked')
+    alert('Explore our travel packages');
+}
